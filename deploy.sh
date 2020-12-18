94505402b77a9076448c09253e70b3357bb96e9f
mak89al <mak89al@outlook.com>
2020-12-18 16:13:08 +0300

    add echo

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 sudo apt-get update
 echo "updated"
 sudo apt-get install -y git
-sudo git clone -b monolith https://github.com/express42/reddit.git
+echo "git installed"
+git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
 ps aux | grep puma
