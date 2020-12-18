299e7fe754ecf2991b9b1fca8165f4dec58e5f9a
mak89al <mak89al@outlook.com>
2020-12-18 15:50:47 +0300

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-sudo apt-get install -y  git
-git clone -b monolith https://github.com/express42/reddit.git
+sudo apt-get install -y git
+cd ~
+sudo git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
+ps aux | grep puma
 echo "app deployed"
