a02b8dfb24a1c9e863b7df310818b0d2f9a507c8
mak89al <mak89al@outlook.com>
2020-12-18 16:02:55 +0300

    add suap apt-get update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt-get install -y git
-cd ~
+sudo apt-get update
+sudo apt-get install -y git
 sudo git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
